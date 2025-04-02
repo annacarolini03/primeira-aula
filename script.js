45991c064a8a47dbaa37690c7afe8d82ae3772e1
annacarolini03 <160248086+annacarolini03@users.noreply.github.com>
2025-04-02 11:01:09 +0000

    a

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
     let tamanhoAtualFonte = 1;
-
-    const aumentarFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
-
-    aumentarFonteBotao.addEventListener('clik', function () {
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
-})
-diminuirFonteBotao.addEventListener('clik', function () {
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-})
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+});
